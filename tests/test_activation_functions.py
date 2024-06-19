3a1946386843d41cd31924b2909f3c274cd95c2d
Jesus Marcos Torero <jesusmarcostorero@gmail.com>
2024-06-19 16:09:40 +0200

    test: I don't know how to do tests

--- tests/test_activation_functions.py
+++ tests/test_activation_functions.py
@@ -1,8 +1,11 @@
-from pyneural.activation_functions import *
+import pyneural.activation_functions as af
 
 datapoints = [-0.5, 0, 0.5]
 
 def test_identity():
-    assert identity(datapoints[0]) == -0.5, "Result should be -0.5"
-    assert identity(datapoints[1]) == 0, "Result should be 0"
-    assert identity(datapoints[2]) == 0.5, "Result should be 0.5"
+    assert af.identity(datapoints[0]) == -0.5, "Result should be -0.5"
+    assert af.identity(datapoints[1]) == 0, "Result should be 0"
+    assert af.identity(datapoints[2]) == 0.5, "Result should be 0.5"
+
+if __name__ == "__main__":
+    test_identity()
